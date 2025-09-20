204d3e16d05bf5b6a65e37fb1ce0697f03d0e2ad
MOmsai <151957606+MOmsai@users.noreply.github.com>
2025-09-20 12:18:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql2');
-
-function getConnection() {
-    return mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "mydb70"
-    });
-}
-
-module.exports = { getConnection };
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'defaultdb',
+    port: process.env.DB_PORT || 3306, // Defaulting to 3306
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+module.exports = { getConnection: () => pool };
